82ca7b63c45729f4368088a5869369b1bec7cc5c
Areiahna <areiahnacooks@yahoo.com>
2020-08-11 03:29:42 -0400

    first commit

---
+++ recipe_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from recipe_app.models import Recipe, Author
+
+def index_view(request):
+    my_recipes = Recipe.objects.all()
+    return render(request, "index.html", {"recipes": my_recipes})
+
+
+def recipe_details(request,recipe_id):
+    my_recipe = Recipe.objects.filter(id=recipe_id).first()
+    my_author = Author.objects.all()
+    return render(request, "recipe_details.html",{"current_recipe":my_recipe, "authors":my_author})
+
+
+def author_details(request,author_id):
+    my_author = Author.objects.filter(id=author_id).first()
+    my_recipes = Recipe.objects.filter(author= my_author)
+    return render(request, "author_details.html",{"current_author":my_author, "recipes": my_recipes})
